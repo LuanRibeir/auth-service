3d419a1f90a63e77251b3180f90afe0d73d787f1
Luan Ribeiro <luaneribeiroalmeida@gmail.com>
2024-02-26 11:13:30 -0300

    Create routers for create and login

--- routes/auth.js
+++ routes/auth.js
@@ -5,5 +5,10 @@ const router = express.Router();
 
 /* GET public route. */
 router.get("/", auth_controller.auth_index);
+// POST request for creating Auth.
+router.post("/create", auth_controller.auth_create_post);
+
+// POST request for login Auth.
+router.post("/login", auth_controller.auth_login_post);
 
 module.exports = router;
